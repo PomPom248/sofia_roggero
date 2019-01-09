667d090bb451a9aa818fd0101c4b27bd28bd5341
PomPom248 <sofiroggero@gmail.com>
2019-01-09 18:23:16 +0100

    added README with endpoints and guidelines

--- server/message.js
+++ server/message.js
@@ -4,20 +4,20 @@ const messageFunction = require('./messageFunction')
 
 messageRouter.post('/messages', (req, res, next) => {
     const { destination, body } = req.body
-    if (!body || !destination) {
+    if (destination == '' || body == '') {
+        res.status(400).json("Missing value")
+    } else if (!body || !destination) {
         res.status(400).json("Missing key")
     }
     else if (typeof destination !== 'string' || typeof body !== 'string') {
+        console.log(destination.length, body.length)
         res.status(400).json("Values must be strings")
-    }
-    else if (destination === '' || body === '') {
-        res.status(400).json("Missing value")
     }
     else if (!destination.includes('@')) {
         res.status(400).json("Destination needs to be an email")
     }
-    else if (destination.length >= 103000 || body.length >= 103000) {
-        res.status(400).json('Payloads are too long')
+    else if (destination.length >= 100 || body.length >= 100) {
+        res.status(400).json('Message is too long')
     }
     else {
         messageFunction.sendMessage(destination, body)
