b0f2c8ecd6a2b95d164e58a89928060b0bae21b3
PomPom248 <sofiroggero@gmail.com>
2019-01-14 22:20:09 +0100

    changed variables from local to "general"

--- server/services/axiosCall/messageCall.js
+++ server/services/axiosCall/messageCall.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 
 const axios = require('axios')
 const axiosRoute = axios.create({
-    baseURL: process.env.AXIOS_LOCAL
+    baseURL: process.env.AXIOS_URL
 })
 //CAMBIAR baseURL dsp pro la AXIOS_URL
+//URL local - AXIOS_LOCAL
 module.exports = {
     sendMessage(destination, body) {
         return axiosRoute.post("/message", { destination, body }, { timeout: 4000 })
