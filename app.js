da1553f63e6fb9a1ed11c867770a932ca627fbfd
PomPom248 <sofiroggero@gmail.com>
2019-01-15 12:40:38 +0100

    added a second db

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const database = require('./server/database')
+// const secondaryDatabase = require('./server/secondaryDatabase')
 
 app.use('/', (bodyParser.json()), require('./server/index'));
-database.datebaseConnection(process.env.MONGO_URL)
-// database.datebaseConnection(process.env.MONGO_LOCAL) LOCAL
+// database.datebaseConnection(process.env.MONGO_URL)
+database.datebaseConnection(process.env.MONGO_LOCAL)
+database.datebaseConnection(process.env.MONGO_TWO)
+
 
 
 app.listen(process.env.PORT, () => { console.log(`listening to port ${process.env.PORT}`) })
